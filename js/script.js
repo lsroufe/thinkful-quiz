147a95b68006e9d9ca2c5f409d9a8c503558a871
Logan Sroufe <lsroufe@gmail.com>
2016-07-08 15:27:20 -0700

    updated html and js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,26 @@
 $( document ).ready(function() {
-    $('#question').text('Which house has the sigil of a Direwolf?');
-    $('#optionsRadios1').append('House Lannister');
+    // $('#optionsRadios1').append('House Lannister');
+    // $('#optionsRadios2').append('House Baratheon');
+    // $('#optionsRadios3').append('House Tyrell');
+    // $('#optionsRadios4').append('House Stark');
+
+    $('input[name="optionsRadiosHN"]').change(function(){
+    	console.log('changed', $(this).parent().text());
+    	$('input[name="optionsRadiosHN"]').parent().removeClass('checked');
+    	$(this).parent().addClass('checked');
+    });
+
+    $('input[name="optionsRadiosCN"]').change(function(){
+    	console.log('changed', $(this).parent().text());
+    	$('input[name="optionsRadiosCN"]').parent().removeClass('checked');
+    	$(this).parent().addClass('checked');
+    });
+
+   var loadHN = function(questionNum) {
+   		
+   	$('#optionsRadiosHN1').append(questions[0].hn[0]);
+
+   }
+
+   loadHN();
 });
