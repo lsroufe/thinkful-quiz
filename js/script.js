1835fbf2e665a631bc556129ec7b62275e2306a7
Logan Sroufe <lsroufe@gmail.com>
2016-07-13 12:34:59 -0700

    Updated comments and js

--- js/script.js
+++ js/script.js
@@ -1,115 +1,49 @@
-
-qmanager = {
-	currentQuestionNumber : 0;
-	correctPoints : 0;
-	questions: loadQuestions();
-	checkAnswer : function () {
-		var hnAnswer = $('#hn').text();
-		var cnAnswer = $('#cn').text();
-		var currentQuestion = this.questions[this.currentQuestionNum]
-		var hnCorrect = currentQuestion.correcthn;
-		var cnCorrect = currentQuestion.correctcn;
-		var questionPic = currQuestion.pic;
-		var questionDesc = currQuestion.desc;
-		function answersScreen(text) {
-			$('#answer-screen').find('h1').text(text).next('img').attr('src', questionPic).next('p').text(questionDesc);
-		}
-
-		if (hnAnswer === hnCorrect && cnAnswer === cnCorrect) {
-			answerScreen("You're Correct");
-			this.correctPoints+=2;
-		}
-
-		else if (hnAnswer === hnCorrect || cnAnswer === cnCorrect) {
-			answerScreen("You're Half-Correct");
-			this.correctPoints+=1;
-		}
-
-		else {
-			answerScreen('Incorrect');
-		};
-	},
-
-	showQuestion : function() {
-		var loadQuestion = function(questionNum) {
-   		//assign the current question in the array to the variable currentQuestion
-   		var currentQuestion = this.questions[this.questionNum];
-   		var questionHN = currentQuestion.hn;
-   		var questionCN = currentQuestion.cn;
-
-   		$('#choices').find('img).attr')
-
-		//for loop that loads the proper answers to both columns based on what question has been loaded
-		for (var i = 0; i < 5; i ++) {
-			$('#optionsRadiosHN'+(i+1)).append(currentQuestion.hn[i]);
-			$('#optionsRadiosCN'+(i+1)).append(currentQuestion.cn[i]);
-		}
-		//load the image based on the current Question			
-		$('#image').attr("src", currentQuestion.pic);
-   }
-//Load the variable currentQuestionNumber and start off on the 1st question
-   var currentQuestionNumber = 0;
-   //use variable loadQuestion to pass the currentQuestionNumber as the 1st question
-   loadQuestion(currentQuestionNumber);
-   //When user clicks the submit button load the function that adds 1 to the currentQuestion number and reloads the function, allowing the next question and answers to appear
-   $('button').click(function(){
-   		currentQuestionNumber++;
-   		loadQuestion(currentQuestionNumber);
-   });
-	}
-}
-
-
-
-
-
-
-
-
-
-
-
-
-//This loads the javascript below
+// This is executed when the page is loaded
 $( document ).ready(function() {
-    // $('#optionsRadios1').append('House Lannister');
-    // $('#optionsRadios2').append('House Baratheon');
-    // $('#optionsRadios3').append('House Tyrell');
-    // $('#optionsRadios4').append('House Stark');
-//Calls the ID optionsRadiosHN to change if a different optionsRadiosHN is selected
+	// This is called / executed when optionsRadiosHN (House names options) a differrnt is clicked
     $('input[name="optionsRadiosHN"]').change(function(){
     	console.log('changed', $(this).parent().text());
     	$('input[name="optionsRadiosHN"]').parent().removeClass('checked');
     	$(this).parent().addClass('checked');
     });
-//Calls the ID optionsRadiosCN to change if a different optionsRadiosCN is selected
+	// This is called / executed when optionsRadiosCN (Character names options) a differrnt is clicked
     $('input[name="optionsRadiosCN"]').change(function(){
     	console.log('changed', $(this).parent().text());
     	$('input[name="optionsRadiosCN"]').parent().removeClass('checked');
     	$(this).parent().addClass('checked');
     });
-//Load the variable loadquestion
-   var loadQuestion = function(questionNum) {
-   		//assign the current question in the array to the variable currentQuestion
-   		var currentQuestion = this.questions[this.questionNum];
-   		var questionHN = currentQuestion.hn;
-   		var questionCN = currentQuestion.cn;
+	
+	// This function loads the House Names and Character names options in the correct HTML elements as well as the image
+    var loadQuestion = function(questionNum) {
+   		// Assign the current question from the array of question objects (questions.js)
+   		var currentQuestion = questions[questionNum];
+   		// var questionHN = currentQuestion.hn;
+   		// var questionCN = currentQuestion.cn;
 
-		//for loop that loads the proper answers to both columns based on what question has been loaded
+
+   		// TODO
+   		// delete or empty the <label> elements before adding the new answers
+   		// generate the HTML <input> to insert inside the <label>
+
+
+		// for loop that loads the proper answers to both columns based on what question has been loaded
+		// set the 2 set of answers for each question
 		for (var i = 0; i < 5; i ++) {
 			$('#optionsRadiosHN'+(i+1)).append(currentQuestion.hn[i]);
 			$('#optionsRadiosCN'+(i+1)).append(currentQuestion.cn[i]);
 		}
-		//load the image based on the current Question			
+		//load the image based on the current Question		
 		$('#image').attr("src", currentQuestion.pic);
-   }
-//Load the variable currentQuestionNumber and start off on the 1st question
-   var currentQuestionNumber = 0;
-   //use variable loadQuestion to pass the currentQuestionNumber as the 1st question
-   loadQuestion(currentQuestionNumber);
-   //When user clicks the submit button load the function that adds 1 to the currentQuestion number and reloads the function, allowing the next question and answers to appear
-   $('button').click(function(){
+    }
+	// Set variable to start off on the 1st question
+    var currentQuestionNumber = 0;
+    
+    // run function passing the currentQuestionNumber, which at the beginnig is the 1st question
+    loadQuestion(currentQuestionNumber);
+
+    //When user clicks the submit button add 1 to the currentQuestion number to allow the next question and answers to appear
+    $('button').click(function(){
    		currentQuestionNumber++;
    		loadQuestion(currentQuestionNumber);
-   });
+    });
 });
